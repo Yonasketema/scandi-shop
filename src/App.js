2f64155e75a71a8d24f6de384ea7ae0a97ef5c1c
yonasketema <yonask.kitaw@gmail.com>
2022-08-15 00:23:57 +0300

    change  categories

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./component/Header";
 import { ProductPageContainer } from "./container/ProductPageContainer";
 import { CartContainer } from "./container/CartContainer";
 import { Center } from "./component/lib";
-import All from "./page/All";
+import Categories from "./page/categories";
 class App extends React.Component {
   render() {
     return (
@@ -18,7 +18,7 @@ class App extends React.Component {
           <Router>
             <Header />
             <Routes>
-              <Route path="/:id" element={<All />} />
+              <Route path="/:id" element={<Categories />} />
               <Route path="/product/:id" element={<ProductPageContainer />} />
               <Route path="/cart" element={<CartContainer large={true} />} />
               <Route path="*" element={<Navigate to="/all" />} />
